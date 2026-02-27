7d8206d07e6f827664aa2312a7acdae16a54e8d4
shahedbitar <145605517+shahedbitar@users.noreply.github.com>
2026-02-27 13:00:04 -0500

    Update vite.config.ts
    fixing to allow github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { createServer } from "./server";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/ICAP-London/",
   server: {
     host: "::",
     port: 8080,
